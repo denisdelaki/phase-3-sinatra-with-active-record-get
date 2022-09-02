231a238dbc55e62437851cf96b721ef8e3f1d6d1
denisdelaki <denisdelaki@gmail.com>
2022-09-02 14:42:41 +0300

    Done.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,11 @@
 class ApplicationController < Sinatra::Base
-
-  get '/' do
-    { message: "Hello world" }.to_json
+ set :default_content_type, 'application/Json'
+  get '/games' do
+    #get all the games from the database
+    games=Game.all
+    #return aJSON response with an array of all the given data
+    games.to_json
+    
   end
 
 end
